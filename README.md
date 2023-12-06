a7bb02288ab463f4f450edb25fd5d44f63ba56bf
Ushnish Tapaswi <126304736+plushexe351@users.noreply.github.com>
2023-12-07 04:59:12 +0530

    Added Google Search and Youtube Search

--- README.md
+++ README.md
@@ -5,11 +5,15 @@
 ## Features
 
 - Beautiful UI
-- Logs History
-
-## Work in Progress
-
 - Opening websites
-- Wikipedia search
-- Weather information
+- OpenAI integration (queries are google searched if no response from OpenAI due to insufficient quota/usage)
+- Fetch most relevant Google search result using Google Custom Search API
+- Searching/Playing Songs on Youtube using Youtube Data Search API
+- Logs History where queries can be revisited
+
+### Work in Progress
+
 - Voice Recognition
+- Weather information
+- Space information
+- Jokes
--- script.js
+++ script.js
@@ -2,7 +2,11 @@ const sendBtn = document.querySelector('.send');
 const container = document.querySelector('.container');
 const userMessage = document.querySelector('#userMessage');
 const sampleQueries = document.querySelectorAll('.container > div')
-const apiKey = "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";
+
+const openAiApiKey = "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";
+const googleSearchApiKey = "AIzaSyDMpl_TDQNh3v4NI7bHe3ArCm5rxE_vk3w";
+const cx = "672ad82a944ee4d61";
+
 const showHistoryBtn = document.querySelector('.fa-history');
 const historyModal = document.querySelector('.history-modal');
 const historyContainerDOM = document.querySelectorAll('.history-container');
@@ -11,19 +15,12 @@ showHistoryBtn.addEventListener('click', () => {
     historyModal.classList.toggle('active');
 })
 
-// historyContainerDOM.forEach(container => {
-//     container.addEventListener('click', () => {
-//         console.log("ok");
-//         historyModal.classList.remove('active');
-//     })
-// })
+historyModal.addEventListener('click', () => {
+    historyModal.classList.remove('active');
 
-// historyModal.addEventListener('click', () => {
-//     historyModal.classList.remove('active');
-// })
-
-
+})
 // Display user or chatbot message 
+
 function displayMessage(user, message) {
 
     const newMessage = document.createElement('div');
@@ -36,7 +33,7 @@ function displayMessage(user, message) {
     timestamp.classList.add('time');
     messageContent.classList.add('message');
     username.textContent = user;
-    messageContent.textContent = message;
+    messageContent.innerHTML = message;
     timestamp.textContent = time;
 
     newMessage.appendChild(username);
@@ -82,7 +79,7 @@ sampleQueries.forEach(query => {
     query.addEventListener('click', () => {
         setTimeout(() => {
             userMessage.value = query.textContent;
-            sendBtn.click();
+            sendMessage();
         }, 300);
     })
 
@@ -107,23 +104,55 @@ async function sendMessage() {
         displayMessage("You", query);
 
         if (query.toLowerCase() == ("hello") || query.toLowerCase() == ("hey") || query.toLowerCase() == ("hi")) {
-            response = "Hello Sir, how may I help you today ?";
+            response = "Hello, how may I help you today ?";
             // setTimeout(() => {
             displayMessage("Euphoria", response);
             // }, 1000);
             return;
         }
 
-        const websites = ["youtube", "google", "wikipedia", "instagram", "facebook", "pinterest"];
 
-        if (query.includes('open')) {
-            websites.forEach(website => {
-                if (query.includes(website))
-                    displayMessage("Euphoria", `Opening ${website}`);
-            })
+        if (query.toLowerCase().includes('play') || (query.toLowerCase().includes('play') && query.toLowerCase().includes('youtube')) || (query.toLowerCase().includes('search') && query.toLowerCase().includes('youtube'))) {
+            const youtubeSearchApiUrl = `https://www.googleapis.com/youtube/v3/search?q=${encodeURIComponent(query)}&part=snippet&key=AIzaSyAFrBrByQtCD-dbImaAkcJIvOwEyaG3eXA`;
+
+            fetch(youtubeSearchApiUrl)
+                .then(response => response.json())
+                .then(data => {
+                    const videoId = data.items[0].id.videoId;
+                    const youtubeVideoUrl = `https://www.youtube.com/watch?v=${videoId}`;
+                    window.open(youtubeVideoUrl, '_blank');
+                    displayMessage("Euphoria", "Playing...")
+                })
+                .catch(error => {
+                    console.error('Error fetching YouTube API:', error);
+                    displayMessage("Euphoria", 'Sorry, there was an error fetching the video.');
+
+                });
+            return;
+        }
+        if (query.toLowerCase().includes('search') || query.toLowerCase().includes('google')) {
+            query = query.replace("search", "").replace("for", "").replace("on", "").replace("google", "");
+            displayMessage("Euphoria", `Searching "${query}"...`);
+            const googleSearchUrl = `https://www.google.com/search?q=${encodeURIComponent(query)}`;
+            setTimeout(() => {
+                window.open(googleSearchUrl, '_blank');
+            }, 700);
             return;
         }
 
+        if (query.toLowerCase().includes('how are you')) {
+            displayMessage('Euphoria', `I'm doing well. What can I help you with?`);
+            return;
+        }
+        if (query.toLowerCase().includes('who are you')) {
+            displayMessage('Euphoria', `I'm Euphoria, a chatBot powered by OpenAI and various other APIs. Is there anything I can help you with ?`);
+            return;
+        }
+
+        if (query.toLowerCase().includes('your name') && query.toLowerCase().includes('what')) {
+            displayMessage("Euphoria", `I'm Euphoria. Is there anything I can help you with?`);
+            return;
+        }
 
         try {
             const modelResponse = await fetchOpenAIResponse(query);
@@ -131,15 +160,41 @@ async function sendMessage() {
             // Display model response in the chat log
 
             displayMessage("Euphoria", modelResponse);
+
         } catch (error) {
-            console.error("Error fetching response:", error);
-            displayMessage(
-                "Euphoria", "Sorry, there was an error fetching the response."
-            );
+
+            console.error("Error fetching response from OpenAI:", error);
+
+            const googleSearchApiUrl = `https://www.googleapis.com/customsearch/v1?q=${encodeURIComponent(query)}&key=${googleSearchApiKey}&cx=${cx}`;
+
+            try {
+                const response = await fetch(googleSearchApiUrl);
+                const data = await response.json();
+
+                if (data.items && data.items.length > 0) {
+
+                    const mostRelevantResult = data.items[0];
+                    const title = mostRelevantResult.title;
+                    const snippet = mostRelevantResult.snippet;
+                    const link = mostRelevantResult.link;
+
+                    console.log(snippet);
+
+
+                    if (query.toLowerCase().includes('open')) {
+                        displayMessage("Euphoria", "Opening...")
+                        window.open(link);
+                        return;
+                    }
+                    displayMessage("Euphoria", `${snippet}` + `<br/>(${title})` + `<br/><a target="_blank" href="${link}"style="color:blueviolet">Click here to view full result on Google</a>`);
+                }
+                // Now "data" contains the search results from Google
+                console.log(data);
+            } catch (error) {
+                console.error('Error fetching search results from Google:', error.message);
+            }
         }
-
-        if (query == "clear") container.innerHTML = "";
-
+        if (query == "clear") { container.innerHTML = ""; return; }
     }
 }
 
@@ -151,7 +206,7 @@ async function fetchOpenAIResponse(prompt) {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
-            Authorization: `Bearer ${apiKey}`,
+            Authorization: `Bearer ${openAiApiKey}`,
         },
         body: JSON.stringify({
             prompt: prompt,
@@ -168,7 +223,8 @@ async function fetchOpenAIResponse(prompt) {
 
 document.addEventListener('keydown', (e) => {
     if (e.key == "Enter") {
-        sendBtn.click();
+        e.preventDefault();
+        sendMessage();
     }
 })
 
