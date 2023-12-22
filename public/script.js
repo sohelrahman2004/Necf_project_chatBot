822c4de5f12c70719341fbc54faf64a10f3e5fc2
Ushnish Tapaswi <126304736+plushexe351@users.noreply.github.com>
2023-12-22 09:42:23 +0530

    pls work

--- public/script.js
+++ public/script.js
@@ -22,7 +22,7 @@ function googleSearch(searchInput) {
         return;
     }
     // Make a POST request to the backend
-    fetch('http://localhost:3000/googleSearch', {
+    fetch('/googleSearch', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -54,7 +54,7 @@ function googleSearch(searchInput) {
 
 // Display user or chatbot message 
 function searchAndPlay(query) {
-    fetch('http://localhost:3000/searchAndPlay', {
+    fetch('/searchAndPlay', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
